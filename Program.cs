ada5dffe9f8358c7186eea9538a1e11de29b6f12
moamen270 <mmoamen270@gmail.com>
2021-09-04 15:54:58 +0200

    Fix Messsage range bug , add new message range method , remove int[] from code (useless)

--- Program.cs
+++ Program.cs
@@ -6,172 +6,188 @@ namespace ConsoleApp3
         // Every thing about the Key 
         public class Key
         {
-            public string sKey;
-            public int[] intKey;
+            public string key;
+           
+
 
             // Constructor to create the key as a string 
-            public Key(string sKey)
+            public Key(string key)
             {
-                this.sKey = sKey;
-                RgCheck();
-                intKey = new int[10];
-                arrMethKey();
+                this.key = key;
+                RangeCheck();
+                FormatCheck();
             }
-            // Method to Re-Entring the Key 
-            public void KeyRe()
+
+
+            // Method to Re-Make the Key 
+            public void KeyRemake()
             {
                 Console.WriteLine("Please Re-Enter The Key");
-                sKey = Console.ReadLine();
-                RgCheck();
+                key = Console.ReadLine();
+                RangeCheck();
 
             }
-            // Checking the range of the key 
-            public void RgCheck()
+
+
+
+            // Check range of the key 
+            public void RangeCheck()
             {
                 // the range is 10 no less no more 
-                if (sKey.Length != 10)
+                if (key.Length != 10)
                 {
                     Console.WriteLine("NOT ALLOWED, ONLY 10 NUMBER FROM 0 TO 9 ");
-                    // if the range less or more than 10 calling Re-Entring Method
-                    KeyRe();
+                    // if the range less or more than 10 call Re-Entring Method
+                    KeyRemake();
                 }
             }
 
-            // Convert StringKey to CharArray then grom CharrArray to intArray
-            public void arrMethKey()
+
+
+            public void FormatCheck()
             {
-                char[] v1 = new char[10];
-                v1 = sKey.ToCharArray();
                 for (int i = 0; i < 10; i++)
-                {   // useing Try-Catch for Run time Error 
-                    // if user entered Characters 
-                    try
-                    {
-                        // useing TOString to convert every single Char to string because Pares take string value only 
-                        // useing Parse to convert from string to int 
-                        intKey[i] = int.Parse(v1[i].ToString());
-                    }
-                    catch
-                    {
-                        // if user enterd an character start the Method
-                        intKeyCheck();
-                    }
+                {
+                    if (!char.IsNumber(key[i]))                       
+                        WrongFormat();
                 }
                 // start Method to Check the duplicate Numbers
                 KeyDupCheck();
             }
+
+
+
             public void KeyDupCheck()
             {
-                int Edup = 0;
+                bool Edup = false;
                 for (int i = 0; i < 10; i++)
                 {
                     for (int j = 9; j > i; j--)
                     {
-                        if (intKey[i] == intKey[j])
+                        if (key[i] == key[j])
                         {
                             // if we have one dublicateError break the loop and do action
-                            Edup = 1;
+                            Edup = true;
                             break;
                         }
                     }
-                    if (Edup == 1) break;
+                    if (Edup == true) break;
 
                 }
-                if (Edup == 1)
+                if (Edup == true)
                 {
                     Console.WriteLine("Not Allowed,Key: duplicate number ");
-                    // Call ReEntring Method then start From the beginning
-                    KeyRe();
-                    arrMethKey();
-
+                    // Call Entring Method then start From the beginning
+                    KeyRemake();
+                    FormatCheck();
                 }
 
             }
 
-            public void intKeyCheck()
+
+
+
+            public void WrongFormat()
             {
                 Console.WriteLine("NOT ALLOWED, Please Enter Numbers Only!");
                 // Call ReEntring Method then start From the beginning
-                KeyRe();
-                arrMethKey();
+                KeyRemake();
+                FormatCheck();
             }
 
 
         }
+
+
+
         //Every thing aboud the Message 
         public class Message
         {
 
-            public string sMess;
-            public int[] intMess;
+            public string message;
 
             // constructor to create the message as a string
-            public Message(string sMess)
+            public Message(string Message)
             {
-                this.sMess = sMess;
-                intMess = new int[sMess.Length];
-                arrMethMess();
+                this.message = Message;
+                RangeCheck();
+                WrongFormatCheck();
+            }
+
+            public void RangeCheck()
+            {
+                // the range is 10 no less no more 
+                if (message.Length % 10 != 0)
+                {
+                    Console.WriteLine("NOT ALLOWED, ONLY 10 NUMBER FROM 0 TO 9 ");
+                    // if the range less or more than 10 call Re-Entring Method
+                    MessageRemake();                   
+                }
             }
             // Method to Re-Entring the Message 
-            public void messRe()
+            public void MessageRemake()
             {
                 Console.WriteLine("Please Re-Enter The Message");
-                sMess = Console.ReadLine();
+                message = Console.ReadLine();
+                RangeCheck();
             }
-            public void arrMethMess()
+
+
+            public void WrongFormatCheck()
+            {
+                for(int i=0; i<message.Length;i++)
+                {
+                    if (!char.IsNumber(message[i]))                                         
+                        WrongFormat();
+                }
+            }
+
+
+            public void WrongFormat()
             {
 
-                char[] v2 = new char[sMess.Length];
-                v2 = sMess.ToCharArray();
-                // useing Try-Catch for Run time Error 
-                // if user entered Characters 
-                try
-                {
-                    for (int i = 0; i < sMess.Length; i++)
-                    {
-                        // useing TOString to convert every single Char to string because Pares take string value only 
-                        // useing Parse to convert from string to int 
-                        string CTS = v2[i].ToString();
-                        intMess[i] = int.Parse(CTS);
-                    }
-                }
-                catch
-                {
-                    // if user enterd an character start the Method
-                    intMessCheck();
-                }
-
-            }
-            public void intMessCheck()
-            {
                 Console.WriteLine("NOT ALLOWED, Please Enter Numbers Only!");
                 // Call ReEntring Method 
-                messRe();
-                arrMethMess();
+                MessageRemake();
+                WrongFormatCheck();
             }
         }
 
         static void Main(string[] args)
         {
             //Rules
-            Console.WriteLine("This is Encryption App");
-            Console.WriteLine("Key and Message should be only Numbers");
-            Console.WriteLine("Key's range is 10 Numbers from 0 to 9, Repetition is not allowed");
-            Console.WriteLine("===================================================================");
+            Console.WriteLine("\tThis is Encryption App");
+            Console.WriteLine("\tKey and Message should be only Numbers");
+            Console.WriteLine("\tKey range is 10 Numbers from 0 to 9, Repetition is not allowed");
+            Console.WriteLine("==========================================================================");
+            
             // Call the constructor and Entring the Key 
-            Console.WriteLine("Enter The Key");
+            Console.WriteLine("\tEnter The Key");
             Key S1 = new Key(Console.ReadLine());
+
+            Console.WriteLine("\n==========================================================================");
+            Console.WriteLine("\tMessage should be only Numbers");
+            Console.WriteLine("\tMessage range shuld be any multiple of 10");
+            Console.WriteLine("==========================================================================");
             // Call the constructor and Entring the Message 
-            Console.WriteLine("Enter the Message");
+            Console.WriteLine("\tEnter the Message");
             Message S2 = new Message(Console.ReadLine());
-            // Prossesing The Message by the Key and The Output
-            int Count = 0;
-            Console.WriteLine("Encrypted Message");
-            for (int i = 0; i < S2.sMess.Length; i++)
+            Console.WriteLine("==========================================================================");
+            // Prossesing The Message by the Key and The Output            
+            Console.WriteLine("\tEncrypted Message");
+            int Counter = 0;
+            int round = 0;
+            int t = 0;
+            for (int i = 0; i < S2.message.Length; i++)
             {
-                if (Count == 10) Count = 0;
-                Console.Write(S2.intMess[S1.intKey[Count]]);
-                Count++;
+                if (Counter > 9)
+                {
+                    Counter = 0;
+                    round++;
+                }
+                t = int.Parse(S1.key[Counter].ToString());
+                Console.Write(S2.message[t+(round*10)]);
+                Counter++;
             }
         }
     }
